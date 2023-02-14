c5f1ac1c6e885e80f80a7f0d2adb949cf931d6b0
Juan Manuel Cruz Badillo <cubj25@gmail.com>
2023-02-13 21:53:12 -0600

    feat(service): Create getAllproducts endpoint with unit test

---
+++ src/main/java/com/code/product/controller/ProductController.java
@@ -0,0 +1,26 @@
+package com.code.product.controller;
+
+import com.code.product.dtos.ProductResponse;
+import com.code.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products/v0/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
+}
---
+++ src/test/java/com/code/product/controller/ProductControllerTest.java
@@ -0,0 +1,44 @@
+package com.code.product.controller;
+
+import jdk.net.SocketFlow;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
+class ProductControllerTest {
+
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.0.10");
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldGetAllProductFromMongoDB() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/products/v0/products")
+        ).andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
